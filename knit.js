3726012b7437a3a7a8072a3a257fbea0ac763494
Alejandro Forero Cuervo <alefore@gmail.com>
2024-12-23 16:50:56 +0100

    Remove redundant declaration.

--- knit.js
+++ knit.js
@@ -1,18 +1,3 @@
-function createConstants(...keys) {
-  const constants = keys.reduce((obj, key) => {
-    obj[key] = key;
-    return obj;
-  }, {});
-  return new Proxy(constants, {
-    get: (target, name) => {
-      if (name in target)
-        return target[name];
-      else
-        throw new Error(`Constant "${String(name)}" is not defined.`);
-    }
-  });
-}
-
 const objectIds = createConstants(
     'configureButton', 'factoryWarnings', 'knitButton', 'buttonNext',
     'buttonPrev');
