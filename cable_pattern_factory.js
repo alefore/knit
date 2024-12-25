093077c22436eb9f72bb57c6a974d5ebb5e9605e
Alejandro Forero Cuervo <alefore@gmail.com>
2024-12-25 22:49:04 +0100

    Define a simple celtic cable.

--- cable_pattern_factory.js
+++ cable_pattern_factory.js
@@ -2,26 +2,129 @@ class CablePatternFactory {
   static name = 'Cables';
 
   constructor() {
-    this.rowsInput = new PatternFactoryInput(
+    this.cableLengthInput = new PatternFactoryInput(
         'Cable Length', 'How long should the pattern be?', 1,
         'cable pattern rows');
-    this.widthInput = new PatternFactoryInput(
-        'Cable Width',
-        'How many times should the basic cable pattern be repeated per row?', 1,
-        'repeats per row');
   }
 
   getInputs() {
     return [
-      this.rowsInput,
-      this.widthInput,
+      this.cableLengthInput,
     ];
   }
 
   build() {
     const output = new Pattern();
-    for (let row = 0; row < this.rowsInput.numberValue(); row++)
-      output.addRow(garterRow(row, this.widthInput.numberValue() * 16));
+    const evenRow = new Row([
+      new StitchSequence([SlipStitchPurlwise], 1),
+      new StitchSequence([StitchEcho], 23)
+    ]);
+    for (let i = 0; i < this.cableLengthInput.numberValue(); i++) {
+      // Row 1
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise, Purl, CableTwoBackKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence(
+            [Purl, Purl, Purl, Purl, CableTwoBackKnitTwo, Knit, Knit], 2),
+        new StitchSequence([Purl], 2)
+      ]));
+      output.addRow(evenRow);
+      // Row 3
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise, CableOneBackKnitTwo, Purl], 1),
+        new StitchSequence(
+            [CableTwoFrontPurlTwo, Knit, Knit, CableTwoBackKnitTwo, Purl, Purl],
+            2),
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 1),
+      ]));
+      output.addRow(evenRow);
+      // Row 5
+      output.addRow(new Row([
+        new StitchSequence([CableOneBackKnitTwo], 1),
+        new StitchSequence([Purl], 4),
+        new StitchSequence([CableTwoFrontKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 4),
+        new StitchSequence([CableTwoFrontKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+      ]));
+      output.addRow(evenRow);
+      // Row 7
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise, Knit], 1),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableOneBackKnitTwo, Purl], 1),
+        new StitchSequence([CableTwoFrontPurlTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([CableTwoBackKnitTwo], 1),
+        new StitchSequence([Purl], 2),
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([Knit], 2),
+      ]));
+      output.addRow(evenRow);
+      // Row 9
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise, Knit], 1),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableTwoBackKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([Knit], 2),
+      ]));
+      output.addRow(evenRow);
+      // Row 11:
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise, Knit], 1),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence(
+            [CableTwoBackKnitTwo, Purl, Purl, CableTwoFrontPurlTwo, Knit, Knit],
+            1),
+        new StitchSequence([CableOneBackKnitTwo], 1),
+        new StitchSequence([Purl], 4),
+        new StitchSequence([Knit], 2),
+      ]));
+      output.addRow(evenRow);
+      // Row 13
+      output.addRow(new Row([
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableTwoFrontKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 4),
+        new StitchSequence([CableTwoFrontKnitTwo], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence([Purl], 3),
+        new StitchSequence([CableOneBackKnitTwo], 1),
+        new StitchSequence([Purl], 1),
+      ]));
+      output.addRow(evenRow);
+      // Row 15
+      output.addRow(new Row([
+        new StitchSequence([SlipStitchPurlwise], 1),
+        new StitchSequence([CableTwoFrontPurlOne], 1),
+        new StitchSequence([Knit], 2),
+        new StitchSequence(
+            [CableTwoBackKnitTwo, Purl, Purl, CableTwoFrontPurlTwo, Knit, Knit],
+            2),
+        new StitchSequence([CableOneBackKnitTwo], 1),
+        new StitchSequence([Purl], 2),
+      ]));
+      output.addRow(evenRow);
+    }
     return output;
   }
 }
--- stitch.js
+++ stitch.js
@@ -26,3 +26,25 @@ const SlipStitchPurlwise =
     new Stitch(1, 1, 'SLP', '#f4cae4', 'SpliStitchPurlwise');
 const KnitTwoTogether = new Stitch(2, 1, 'K2Tog', '#e6f5c9', 'KnitTwoTogether');
 const WithYarnInFront = new Stitch(0, 0, 'WYIF', '#f4cae4', 'WithYarnInFront');
+
+// Cable stitches
+// Order: slip count, [front, back], stitch count, [knit, purl]
+const CableOneBackKnitTwo = new Stitch(
+    2, 2, 'CBK', '#f4cae4',
+    'Slip 1 onto cable, hold in back, knit 2, return from cable to left needle');
+const CableTwoFrontPurlOne = new Stitch(
+    1, 1, 'C2FP', '#f4cae4',
+    'Slip 2 onto cable, hold in front, purl 1, return from cable to left needle');
+const CableTwoFrontKnitTwo = new Stitch(
+    2, 2, 'C2FK2', '#f4cae4',
+    'Slip 2 onto cable, hold in front, knit 2, return from cable to left needle');
+const CableTwoFrontPurlTwo = new Stitch(
+    2, 2, 'C2FP2', '#f4cae4',
+    'Slip 2 onto cable, hold in front, purl 2, return from cable to left needle');
+const CableTwoBackKnitTwo = new Stitch(
+    2, 2, 'C2BK2', '#f4cae4',
+    'Slip 2 onto cable, hold in back, knit 2, return from cable to left needle');
+
+// Other stitches
+const StitchEcho =
+    new Stitch(1, 1, 'R', '#b3e2cd', 'Knit the knits and purl the purls');
