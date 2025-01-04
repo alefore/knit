174e29d2872f84d990fcb79ac299a9bbecacda45
Alejandro Forero Cuervo <alefore@gmail.com>
2025-01-04 16:31:55 +0100

    Drastically rework the cables.

--- cable_pattern_factory.js
+++ cable_pattern_factory.js
@@ -1,3 +1,119 @@
+// 33     11          22      44
+// 33     11          22      44
+//  33     11        22      44
+//   33      11    22      44
+//     33      1122      44
+//       33    2211    44
+//         3322    1144
+//         2233    4411
+//        22   3344   11
+//        22   4433   11
+//         2244    3311
+//           22    1133
+//
+//
+//
+//  11   5533    4422   66
+// 11    3355    2244    66
+// 11   33   5522   44   66
+// 11   33   2255   44   66
+// 11    3322    5544    66
+//  11   2233    4455   66
+//   1122    3344    5566
+//   2211    4433    6655
+//
+//---
+// 11
+// 11
+//  11   22
+//   1122
+//   2211
+//  22  11
+// 22    11
+// 22    11
+//      11
+//     11
+
+// 11
+//  11
+//    11
+//     11
+//     11
+//    11
+//  11
+// 11
+class CableLayout {
+  // @param {number} cableEdgeRows - The number of rows on the edge where the
+  // cable stays without moving. Typically 2.
+  // @param {number} smoothingRows - The number of rows around the edge where
+  // the cable only jumps 1 stitch (per row). Typically 1 or 2.
+  constructor(cables, rowsBetweenCables, cableEdgeRows, smoothingRows) {
+    // rowsBetweenCables: On the side, how much empty space should we leave
+    // after one cable leaves the side before the next cable reaches it?
+    this.cables = cables;
+    this.cableWidth = 2;  // Stitches.
+    this.rowsBetweenCables = rowsBetweenCables;
+    this.cableEdgeRows = cableEdgeRows;
+    this.smoothingRows = smoothingRows;
+    this.rowsCount = cables * (this.cableEdgeRows + this.rowsBetweenCables);
+    this.rowsWidth = this.#rowsWidth();
+    console.log(this.rowsWidth);
+    // positions[row][cable] stores the earliest position of the given cable at
+    // a given row.
+    this.positions = this.#cablePositions();
+  }
+
+  #rowsWidth() {
+    // If we have more than 4 rows, compute all the moves the first cable can
+    // make before reaching the end (and having to bounce back at half of the
+    // rows).
+    console.log(this.rowsCount);
+    const moveRows = 1 + this.rowsCount / 2 - this.cableEdgeRows;
+    const singleMoveRows = Math.min(moveRows, 2 * this.smoothingRows);
+    const doubleMoveRows = moveRows - singleMoveRows;
+    console.log(moveRows);
+    console.log(singleMoveRows);
+    console.log(doubleMoveRows);
+    return this.cableWidth + singleMoveRows + 2 * doubleMoveRows;
+  }
+
+  #cablePositions() {
+    const output =
+        Array.from({length: this.rowsCount}, () => Array(this.cables).fill(-1))
+    for (let cable = 0; cable < this.cables; cable++) {
+      const cableStartRow =
+          cable * (this.cableEdgeRows + this.rowsBetweenCables);
+      let position = 0;
+      for (let row = 0; row < this.rowsCount / 2; row++) {
+        output[(cableStartRow + row) % this.rowsCount][cable] = position;
+        output[(cableStartRow + this.rowsCount / 2 + row) % this.rowsCount][cable] =
+            this.rowsWidth - position - this.cableWidth;
+        if (row < this.cableEdgeRows - 1)
+          continue;
+        else if (
+            (row + 1 >= this.cableEdgeRows &&
+             row + 1 < this.cableEdgeRows + this.smoothingRows) ||
+            row + 1 > this.rowsCount / 2 - this.smoothingRows)
+          position += 1;
+        else
+          position += 2;
+      }
+    }
+    console.log(output);
+    return output;
+  }
+
+  renderRowCables(row) {
+    const output = Array(this.rowsWidth).fill(-1);
+    for (let cable = 0; cable < this.cables; cable++) {
+      output[this.positions[row][cable]] = cable;
+      output[this.positions[row][cable] + 1] = cable;
+    }
+    console.log(output);
+    return output;
+  }
+}
+
 class CablePatternFactory {
   static name = 'Cables';
 
@@ -5,15 +121,92 @@ class CablePatternFactory {
     this.cableLengthInput = new PatternFactoryInput(
         'Cable Length', 'How long should the pattern be?', 1,
         'cable pattern rows');
+    this.cableCountInput = new PatternFactoryInput(
+        'Cable Count', 'How many cables do you want?', 5, 'cables');
+    this.rowsBetweenCablesInput = new PatternFactoryInput(
+        'Rows Between Cables', 'How many rows between cables?', 2, 'rows');
+    this.cableEdgeRowsInput = new PatternFactoryInput(
+        'Cable edge rows', 'Length of the edge of each cable', 2, 'rows');
+    this.smoothingRowsInput = new PatternFactoryInput(
+        'Single-stitch jump rows',
+        'For how many rows should cables only jump a single stitch?', 1,
+        'rows');
   }
 
   getInputs() {
     return [
       this.cableLengthInput,
+      this.cableCountInput,
+      this.rowsBetweenCablesInput,
+      this.cableEdgeRowsInput,
+      this.smoothingRowsInput,
     ];
   }
 
   build() {
+    const output = new Pattern();
+    const layout = new CableLayout(
+        this.cableCountInput.numberValue(),
+        this.rowsBetweenCablesInput.numberValue(),
+        this.cableEdgeRowsInput.numberValue(),
+        this.smoothingRowsInput.numberValue());
+    const evenRow =
+        new Row([new StitchSequence([StitchEcho], layout.rowsWidth)]);
+    for (let row = 0; row < layout.rowsCount; row++) {
+      const state = layout.renderRowCables(row);
+      const stateNext = layout.renderRowCables((row + 1) % layout.rowsCount);
+      const rowOutput = [];
+      let stitch = layout.rowsWidth - 1;
+      while (stitch >= 0) {
+        if (state[stitch] == -1 && stateNext[stitch] == -1) {
+          rowOutput.push(new StitchSequence([Purl], 1));
+          stitch--;
+        } else if (
+            stitch > 0 && state[stitch] == stateNext[stitch] &&
+            state[stitch - 1] == stateNext[stitch - 1]) {
+          rowOutput.push(new StitchSequence([Knit], 2));
+          stitch -= 2;
+        } else if (
+            stitch >= 2 && state[stitch] != -1 && stateNext[stitch] == -1 &&
+            state[stitch] == stateNext[stitch - 1]) {
+          rowOutput.push(new StitchSequence([CableTwoFrontPurlOne], 1));
+          rowOutput.push(new StitchSequence([Knit], 2));
+          stitch -= 3;
+        } else if (
+            stitch >= 2 && state[stitch] == -1 && stateNext[stitch] != -1 &&
+            state[stitch - 1] == stateNext[stitch]) {
+          rowOutput.push(new StitchSequence([CableOneBackKnitTwo, Purl], 1));
+          stitch -= 3;
+        } else if (
+            stitch >= 3 && state[stitch] != -1 && state[stitch - 2] != -1 &&
+            state[stitch - 2] == stateNext[stitch] &&
+            state[stitch] == stateNext[stitch - 3]) {
+          rowOutput.push(new StitchSequence([CableTwoBackKnitTwo], 1));
+          rowOutput.push(new StitchSequence([Knit], 2));
+          stitch -= 4;
+        } else if (
+            stitch >= 3 && state[stitch] != -1 && state[stitch - 2] == -1 &&
+            state[stitch] == stateNext[stitch - 2]) {
+          rowOutput.push(new StitchSequence([CableTwoFrontPurlTwo], 1));
+          rowOutput.push(new StitchSequence([Knit], 2));
+          stitch -= 4;
+        } else if (
+            stitch >= 3 && state[stitch] == -1 && state[stitch - 2] != -1 &&
+            stateNext[stitch] == state[stitch - 2]) {
+          rowOutput.push(new StitchSequence([CableTwoBackKnitTwo], 1));
+          rowOutput.push(new StitchSequence([Purl], 2));
+          stitch -= 4;
+        } else {
+          throw new Error('Unhandled cable cross situation');
+        }
+      }
+      output.addRow(new Row(rowOutput));
+      output.addRow(evenRow);
+    }
+    return output;
+  }
+
+  buildOld() {
     const output = new Pattern();
     const evenRow = new Row([
       new StitchSequence([WithYarnInFront, SlipStitchPurlwise], 1),
