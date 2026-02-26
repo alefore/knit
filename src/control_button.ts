a69721b89110db9509fbe5571c2438569dfaa573
Alejandro Forero Cuervo <alefore@gmail.com>
2026-02-26 21:23:52 +0100

    Import missing source.

---
+++ src/control_button.ts
@@ -0,0 +1,36 @@
+import {htmlInputTypes, htmlTags} from './constants.js';
+import {EventListener} from './listener.js';
+
+export class ControlButton {
+  private text: string;
+  private description: string;
+  private htmlObject: HTMLInputElement;
+
+  constructor(
+      id: string|null, text: string, description: string,
+      action: (e: MouseEvent) => void) {
+    this.text = text;
+    this.description = description;
+    const button = document.createElement(htmlTags.input) as HTMLInputElement;
+    button.type = htmlInputTypes.submit;
+    button.value = this.text;
+    button.title = this.description;
+    if (id) {
+      button.id = id;
+    }
+    button.addEventListener('click', action);
+    this.htmlObject = button;
+  }
+
+  appendHtml(container: HTMLElement): this {
+    container.appendChild(this.htmlObject);
+    return this;
+  }
+
+  setEnabled(stateListener: EventListener, value: () => boolean): void {
+    const htmlObject = this.htmlObject;
+    stateListener.addListener(() => {
+      htmlObject.disabled = !value();
+    });
+  }
+}
